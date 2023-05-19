ec93b41f554381e805a4c05e2aa87b25f2ebd658
jameleu <jleung2101@gmail.com>
2023-05-18 23:04:27 -0700

    added reason animations

--- components/intro_bio.jsx
+++ components/intro_bio.jsx
@@ -8,6 +8,8 @@ export default function Intro_Bio() {
     //console.log("top: " + parent_div.current.offsetTop);
     const intro_bio_ref = useRef();
 
+    const intro_bio_ref2 = useRef();
+
     useEffect( () => {
 
         const handle_scroll = () => {
@@ -17,21 +19,28 @@ export default function Intro_Bio() {
                                 
                 //window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
             );
+            document.body.style.setProperty(
+                "--curr_scroll_2",
+                (window.pageYOffset - intro_bio_ref2.current.offsetTop + window.outerHeight) / (intro_bio_ref2.current.offsetTop - window.outerHeight - 1.35*window.innerHeight)
+            );
         };
         window.addEventListener("scroll", handle_scroll);
       return () => window.removeEventListener("scroll", handle_scroll);
     }, []);
       
     return(
+        <>
         <div ref={intro_bio_ref} className={styles.headers}>
             <p className={styles.header_school_R}> I'm a student at the </p>
             <p className={styles.header_school_L}> University of Michigan (Ann Arbor)</p>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
         </div>
+        <div ref={intro_bio_ref2} className={styles.headers2}>
+            <div className={styles.header_reason_T}> I'm interested in full stack development</div>
+            <div className={styles.shadow_T}>  </div>
+            <div className={styles.header_reason_B}> to automate and faciilitate daily life with scalable user-driven software.</div>
+            <div className={styles.shadow_B}> </div>
+        </div>
+        </>
     );
 
 }
