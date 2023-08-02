76b76925412f8397049821045e29861053ba38d0
jameleu <jleung2101@gmail.com>
2023-08-02 01:11:11 -0700

    deleted excess files

--- components/card_list.jsx
+++
@@ -1,7 +0,0 @@
-export const exp_card_list = [
-    {hdr: "test_header1", text: "TESTTESTSTESTSTETSTESTEST TEST TEST TEST"},
-    {hdr: "test_header2", text: "TESTTESTSTESTSTETSTESTEST TEST TEST TEST"},
-    {hdr: "test_header3", text: "TESTTESTSTESTSTETSTESTEST TEST TEST TEST"},
-    {hdr: "test_header4", text: "TESTTESTSTESTSTETSTESTEST TEST TEST TEST"},
-    {hdr: "test_header5", text: "TESTTESTSTESTSTETSTESTEST TEST TEST TEST"},
-];
