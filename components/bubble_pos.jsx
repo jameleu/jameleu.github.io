e5d7ad6e8f10c754d2d65019b674b350662895cb
jameleu <jleung2101@gmail.com>
2023-08-03 03:04:49 -0700

    added text bubble heights

--- components/bubble_pos.jsx
+++ components/bubble_pos.jsx
@@ -5,7 +5,7 @@ export const bubbles = [
       y: 55,
       skill: "Git",
       desc: "In: team work flow, merging and merge requests",
-      box_height: "15vw"
+      box_height: "12vmax"
     },
     {
       s: 0.6,
@@ -13,7 +13,7 @@ export const bubbles = [
       y: 226,
       skill: "Team player",
       desc: "In: everything I do; I'm an active listener, leader, and idea pitcher",
-      box_height: "15vw"
+      box_height: "12vmax"
     },
     {
       s: 0.6,
@@ -21,7 +21,7 @@ export const bubbles = [
       y: 256,
       skill: "DS/Algo",
       desc: "In: UMich classes and 100+ Leetcode problems",
-      box_height: "15vw"
+      box_height: "12vmax"
 
     },
     {
@@ -29,8 +29,8 @@ export const bubbles = [
       x: 1150,
       y: 365,
       skill: "mySQL",
-      desc: "In: Northrop Grumman and shopping website project",
-      box_height: "15vw"
+      desc: "In: Northrop Grumman test bots and shopping website project",
+      box_height: "12vmax"
 
     },
     {
@@ -39,7 +39,7 @@ export const bubbles = [
       y: 193,
       skill: "Java",
       desc: "In: practice with Leetcode and interview problems in class",
-      box_height: "15vw"
+      box_height: "12vmax"
 
     },
     {
@@ -47,8 +47,8 @@ export const bubbles = [
       x: 907,
       y: 88,
       skill: "Regexp",
-      desc: "In: NG test validation retrieval, XML corrector",
-      box_height: "15vw"
+      desc: "In: Northrop Grumman test bot validation retrieval, XML corrector",
+      box_height: "14vmax"
 
     },
     {
@@ -56,8 +56,8 @@ export const bubbles = [
       x: 633,
       y: 320,
       skill: "Javascript",
-      desc: "In: MRobosub website, personal website",
-      box_height: "15vw"
+      desc: "In: MRobosub website, personal website, online classes",
+      box_height: "11vmax"
 
       },
     {
@@ -66,7 +66,7 @@ export const bubbles = [
       y: 60,
       skill: "OOP PHP",
       desc: "In: shopping sorted website with public/admin mode",
-      box_height: "15vw"
+      box_height: "14vmax"
 
     },
     {
@@ -75,7 +75,7 @@ export const bubbles = [
       y: 357,
       skill: "Next/Node.js",
       desc: "In: optimizing websites I've made to refresh and load data faster",
-      box_height: "15vw"
+      box_height: "14vmax"
     
     },
     {
@@ -83,8 +83,8 @@ export const bubbles = [
       x: 1071,
       y: 233,
       skill: "CSS",
-      desc: "In: screen-dependent sizing/placing, animations of my websites",
-      box_height: "15vw"
+      desc: "In: screen-dependent sizing and placing, animations of my websites",
+      box_height: "14vmax"
 
     },
     {
@@ -93,7 +93,7 @@ export const bubbles = [
       y: 365,
       skill: "React",
       desc: "In: my websites to update states, animate, and manage DOM references",
-      box_height: "15vw"
+      box_height: "14vmax"
     
     },
     {
@@ -102,7 +102,7 @@ export const bubbles = [
       y: 111,
       skill: "C++/C",
       desc: "In: BST, OOP, hash table, stack/queue, cache, pipelining projects",
-      box_height: "28vmin"
+      box_height: "14vmax"
 
     },
     {
@@ -110,8 +110,8 @@ export const bubbles = [
       x: 75,
       y: 103,
       skill: "Leadership",
-      desc: "From: head lead of baking club, lead of engineering projects, becoming Eagle Scout",
-      box_height: "18vw"
+      desc: "From: head lead of baking club, lead of engineering projects, and Eagle Scout",
+      box_height: "15vmax"
     
     },
     {
@@ -119,8 +119,8 @@ export const bubbles = [
       x: 413,
       y: 367,
       skill: "Python",
-      desc: "In: Northrop Grumman and MRobosub: bots, scripts, ML",
-      box_height: "24vmin"
+      desc: "In: Northrop Grumman and MRobosub: automation testing, scripts, machine learning",
+      box_height: "15vmax"
       
     },
   ];
